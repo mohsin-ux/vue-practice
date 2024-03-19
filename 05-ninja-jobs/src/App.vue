aa0f189e228bfc922e82355547e6c13cdca06510
Mohsin Razi <mohsinrazakhan4455@gmail.com>
2024-03-19 16:02:20 +0500

    routes parameters

---
+++ 05-ninja-jobs/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav>
+    <router-link :to="{ name: 'home' }">Home</router-link> |
+    <router-link :to="{ name: 'about' }">About</router-link>|
+    <router-link :to="{ name: 'jobs' }">Jobs</router-link>
+  </nav>
+  <router-view />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: white;
+  padding: 10px;
+  background: crimson;
+  border-radius: 10px;
+  text-decoration: none;
+}
+</style>
---
+++ 05-ninja-jobs/src/views/jobs/Jobs.vue
@@ -0,0 +1,23 @@
+<template>
+   <h1>Jobs</h1>
+   <div v-for="job in jobs" :key="jobs.id">
+      <h1> {{ job.title }}</h1>
+   </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jobs: [
+        { title: "Ninja UX Designer", id: 1, details: "lorem" },
+        { title: "Ninja Web Developer", id: 1, details: "lorem" },
+        { title: "Ninja Vue Designer", id: 1, details: "lorem" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
