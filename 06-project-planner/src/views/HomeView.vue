401403f6bad6b1386333ab663eafc2985dbb7a9c
Mohsin Raazi <mohsinrazakhan4455@gmail.com>
2024-04-17 14:10:23 +0000

    create initial setup and a json server for dynamic data

--- 06-project-planner/src/views/HomeView.vue
+++ 06-project-planner/src/views/HomeView.vue
@@ -1,9 +1,32 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+      <p>{{ project.title }}</p>
+    </div>
+
+
+    </div>
+  </div>
 </template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'HomeView',
+  components: {},
+  data(){
+    return {
+      projects: []
+    }
+  },
+  mounted(){
+    fetch('http://localhost:3000/projects')
+      .then(res => res.json())
+      .then(data => this.projects = data)
+      .catch(err => console.log(err.message))
+  }
+}
+</script>
